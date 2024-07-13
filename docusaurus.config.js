d2a441579334e2d945f7abc51408a2965fa9edcf
TNTsky <TNTsky.com@gmail.com>
2024-07-13 14:22:49 +0800

    chore: Update docusaurus.config.js with correct URL and project details

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://TNTsky.github.io',
+  url: 'https://docs.spksmp.top/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/SPKsmp-Docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
