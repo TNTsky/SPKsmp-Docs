94c805531eccb8caa837ad156aedbb195decf21c
TNTsky <TNTsky.com@gmail.com>
2024-07-26 09:27:42 +0800

    Update project logo image path in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/logo.webp',
+      image: 'img/logooo.webp',
       navbar: {
         title: 'Sparkle 花火伺服器指南',
         logo: {
