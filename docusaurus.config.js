051a4fcf54d1c0ce321cc898b29a8a513bd3c11e
TNTsky <92364781+TNTsky@users.noreply.github.com>
2024-07-27 19:29:48 +0800

    Show Last Update Time

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,6 +40,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
+          showLastUpdateTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -47,6 +48,7 @@ const config = {
         },
         blog: {
           showReadingTime: false,
+          showLastUpdateTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
